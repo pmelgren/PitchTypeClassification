b26a021b7144e73d79543fa28cc6ae9c989cbda4
Pete Melgren <pmelgren@gmail.com>
2020-04-03 18:49:31 -0400

    Update classification app.py to use sql queries instead of pre-loaded data. Load queries from functions instead of in-line user pitcher names in drop-down

--- python/classification_app.py
+++ python/classification_app.py
@@ -1,20 +1,47 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import sqlalchemy as sa
 
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import scale
 
-all_data = pd.read_csv('./2018_trackman.csv')
+def query_data(pitcher_id):
+    
+    query_raw = """select velocity, spin_rate, break_x, break_z
+                    from tm.pitch p inner join tm.pitch_measures pm
+                    on pm.pitch_id = p.pitch_id
+                    where pitcher_id = {}"""
+    query_text = query_raw.format(pitcher_id)
+    
+    # create an SQL Alchemy engine and query the data
+    conn_string = 'postgresql://postgres:Melgren1224@localhost:5432/Baseball'
+    engine = sa.create_engine(conn_string)
+    tm = pd.read_sql_query(query_text,engine,coerce_float = False)
+            
+    return(tm)
 
-pitchers = all_data.groupby('pitcher').count()
-pitchers = pitchers[pitchers.p_hand >= 200]
-pitchers = pitchers.sort_values('p_hand',ascending = False).index
+def query_pitchers():
+    
+    query_text = """select distinct name_first || ' ' || name_last as name
+                        ,cast(pitcher_id as int) id
+                    from tm.pitch pt inner join mlb.players py 
+                        on py.player_id = pt.pitcher_id"""
+    
+    # create an SQL Alchemy engine and query the data
+    conn_string = 'postgresql://postgres:Melgren1224@localhost:5432/Baseball'
+    engine = sa.create_engine(conn_string)
+    pl = pd.read_sql_query(query_text,engine,coerce_float = False)
+            
+    return(pl)    
 
-pit = st.sidebar.selectbox('select pitcher',options = pitchers)
 
-tm = all_data[all_data.pitcher == pit]
-tm = tm.loc[:,['velocity','spin_rate','horz_break','vert_break']].dropna()
+pl = query_pitchers()
+
+pit = st.sidebar.selectbox('select pitcher',options = pl.name)
+
+tm = query_data(pl.loc[pl.name == pit,'id'].values[0])
+tm = tm.dropna()
 
 norm = st.sidebar.checkbox('Normalize Data',value = False)
 
@@ -36,13 +63,13 @@ plt.subplot(2,2,1)
 plt.scatter(tm.velocity,tm.spin_rate,c=tm.cluster,cmap = "Set1")
 plt.title('velo vs spin')
 plt.subplot(2,2,3)
-plt.scatter(tm.horz_break,tm.vert_break,c=tm.cluster,cmap = "Set1")
+plt.scatter(tm.break_x,tm.break_z,c=tm.cluster,cmap = "Set1")
 plt.title('hbreak vs vbreak')
 plt.subplot(2,2,4)
-plt.scatter(tm.horz_break,tm.velocity,c=tm.cluster,cmap = "Set1")
+plt.scatter(tm.break_x,tm.velocity,c=tm.cluster,cmap = "Set1")
 plt.title('hbreak vs velo')
 plt.subplot(2,2,2)
-plt.scatter(tm.horz_break,tm.spin_rate,c=tm.cluster,cmap = "Set1")
+plt.scatter(tm.break_x,tm.spin_rate,c=tm.cluster,cmap = "Set1")
 plt.title('hbreak vs spin')
 plt.subplots_adjust(hspace=0.4)
 
